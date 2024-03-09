fa3ae6bbcad5279930eafe77fd4c78a637d669cf
Dhanush <dhanushpakala123@gmail.com>
2024-03-09 11:17:29 +0530

    Created Models

--- src/app/models/Doctor.ts
+++ src/app/models/Doctor.ts
@@ -1,4 +1,4 @@
 export class Doctor{
     constructor(private id: number,private name: string,private specialization:string,private experience:string,private mobileNumber:number,private consultancyFee:number,private isActive:boolean,private email:string,private password:string){}
-
+    
 }
